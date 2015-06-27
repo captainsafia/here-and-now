3f05deed5a8a97f1f067832ff3bc9280c5bae42c
Safia Abdalla <seabdalla@gmail.com>
2015-06-27 10:40:28 -0700

    Added functionality to drop pins on map

--- client/epollclient.js
+++ client/epollclient.js
@@ -5,12 +5,27 @@ if (Meteor.isClient) {
 
   Template.map.helpers({
     mapOptions: function() {
-      if (GoogleMaps.loaded()) {
+      if (GoogleMaps.loaded() && navigator.geolocation) {
         return {
-          center: new google.maps.LatLng(-37.8136, 144.9631),
-          zoom: 8
+          center: new google.maps.LatLng(37.777220,-122.391285),
+          zoom: 18
         };
       }
     }
-  })
+  });
+
+  Template.map.onCreated(function() {
+    GoogleMaps.ready("map", function(map) {
+      google.maps.event.addListener(map.instance, "click", function(event) {
+        var pin = new google.maps.LatLng(
+          event.latLng.lat(), event.latLng.lng());
+        var marker = new google.maps.Marker({
+          draggable: true,
+          animation: google.maps.Animation.DROP,
+          position: pin,
+          map: map.instance
+        })
+      });
+    });
+  });
 }
