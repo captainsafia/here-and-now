e88707562a7e595d8e4150397aeaa4911e1a5761
Safia Abdalla <seabdalla@gmail.com>
2015-06-27 11:59:41 -0700

    Added reactive map marker functionality

--- client/epollclient.js
+++ client/epollclient.js
@@ -3,6 +3,8 @@ if (Meteor.isClient) {
     GoogleMaps.load();
   });
 
+  Markers = new Mongo.Collection("markers");
+
   Template.map.helpers({
     mapOptions: function() {
       if (GoogleMaps.loaded() && navigator.geolocation) {
@@ -15,16 +17,67 @@ if (Meteor.isClient) {
   });
 
   Template.map.onCreated(function() {
+    var markers = {};
+
     GoogleMaps.ready("map", function(map) {
       google.maps.event.addListener(map.instance, "click", function(event) {
-        var pin = new google.maps.LatLng(
-          event.latLng.lat(), event.latLng.lng());
-        var marker = new google.maps.Marker({
-          draggable: true,
-          animation: google.maps.Animation.DROP,
-          position: pin,
-          map: map.instance
-        })
+        console.log(event);
+        // Insert location where the pin was dropped into the Markers collection
+        Markers.insert({
+          lat: event.latLng.lat(),
+          lng: event.latLng.lng()
+        });
+      });
+
+      Markers.find().observe({
+        /*
+        * When a Marker element is added to the Collection, create
+        * a Marker DOM obect for it using the Google Maps API and
+        * add a listener so that it can be updated when it is moved.
+        * Then add it to an object literal that will keep track of the
+        * Marker elements that we have on the front-end.
+        */
+        added: function(document) {
+          var pin = new google.maps.LatLng(document.lat, document.lng);
+          var marker = new google.maps.Marker({
+            draggable: true,
+            animation: google.maps.Animation.DROP,
+            position: pin,
+            map: map.instance,
+            id: document._id
+          });
+
+          google.maps.event.addListener(marker, "dragend", function(event) {
+            Markers.update(marker.id, {
+              $set: {
+                lat: event.latLng.lat(),
+                lng: event.latLng.lng()
+              }
+            });
+          });
+          markers[document._id] = marker;
+        },
+
+        /*
+        * When a Marker element is updated in the collection, get the corresponding
+        * DOM element in the front-end and change its position.
+        */
+        changed: function(old, new_) {
+          markers[new_._id].setPosition({
+            lat: new_.lat,
+            lng: new_.lng
+          });
+        },
+
+        /*
+        * When a Marker element is deleted, remove all instances of it and remove
+        * its listener.
+        */
+        removed: function(old) {
+          markers[old._id].setMap(null);
+          google.maps.event.clearInstanceListeners(markers[old._id]);
+          delete markers[old._id];
+        }
       });
     });
   });
