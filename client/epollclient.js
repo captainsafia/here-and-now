508a75d6a4894bfbf4b4bdbf5880bccb64707ca4
kavyagundavaram <gundava2@illinois.edu>
2015-06-27 15:32:24 -0700

    image functionality

--- client/epollclient.js
+++ client/epollclient.js
@@ -54,6 +54,25 @@ Images.allow({
  }
 });
   
+  Template.Profile.events({
+   ‘change .myFileInput’: function(event, template) {
+      FS.Utility.eachFile(event, function(file) {
+        Images.insert(file, function (err, fileObj) {
+          if (err){
+             // handle error
+          } else {
+             // handle success depending what you need to do
+            var userId = Meteor.userId();
+            var imagesURL = {
+              “profile.image”: “/cfs/files/images/“ + fileObj._id
+            };
+            Meteor.users.update(userId, {$set: imagesURL});
+          }
+        });
+     });
+   },
+
+   
   Meteor.publish(“images”, function(){ return Images.find(); });
 
   Router.route(‘/profile’,{
@@ -68,7 +87,7 @@ Images.allow({
  }
 });
 
-  
+
   Markers = new Mongo.Collection("markers");
 
   Template.map.helpers({
